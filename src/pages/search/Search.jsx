44af3e06cdcbe9184f68863c7681ff23034b9047
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-30 00:56:57 +0530

    dark/light mode context and dark css added

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -6,6 +6,7 @@ import useFetch from "../../hooks/useFetch";
 
 // componenets
 import RecipeList from "../../Components/Recipe List/RecipeList"
+import useTheme from "../../hooks/useTheme";
 export default function Search() {
   const queryString = useLocation().search;
   const queryParams = new URLSearchParams(queryString);
@@ -14,11 +15,12 @@ export default function Search() {
   const url = "http://localhost:3000/recipes?q="+query;
 
   const {error, isPending, data} = useFetch(url);
+  const {mode} = useTheme()
 
   console.log(data, error, isPending);
 
   return (
-    <div>
+    <div className={`search ${mode}`}>
     <h2 className="page-title">Recipes including "{query}"</h2>
         {error && <p className="error">{error}</p>}
     {isPending && <p className="loading">Loading ...</p>}
