b1ab36e39525449f82dee13c15910f2aeb51bc5b
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-30 00:44:30 +0530

    useTheme hook, color context  added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
+// style
 import "./Navbar.css";
+
 import { Link } from "react-router-dom";
+import useTheme from "../../hooks/useTheme";
+
+// components
 import SearchBar from "../Search Bar/SearchBar";
+
+
+
 export default function Navbar() {
+  
+  const {color} = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{background:color}}>
       <nav>
         <Link to="/" className="brand">
           <h1> Cooking Ninja</h1>
