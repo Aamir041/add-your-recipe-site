1c209db3b0839e9c079d27dea2895823c28b0ffa
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-30 00:48:34 +0530

    custom hook for getting context object from ThemeContext

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+ 
+ export default function useTheme() {
+    const context = useContext(ThemeContext);
+    
+    if(context === undefined){
+        throw new Error("useTheme() must be inside ThemeProvider.");
+    }
+
+    return context;
+ }
+ 
